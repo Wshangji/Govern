93b8cc8ee9ed84f25182d003159bf243f0a9d614
TongWY <2463509050@qq.com>
2021-11-20 17:15:21 +0800

    init springboot project

---
+++ governServe/src/main/java/com/tongwy/governserve/GovernServeApplication.java
@@ -0,0 +1,13 @@
+package com.tongwy.governserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GovernServeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GovernServeApplication.class, args);
+    }
+
+}
---
+++ governServe/src/test/java/com/tongwy/governserve/GovernServeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tongwy.governserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GovernServeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
